610b5eb9dbba32c1f51fd4c6a7b2fb93da942bed
Abdullah Kardaş <krdsabdllh53@gmail.com>
2024-06-18 18:58:24 +0300

    Add files via upload

---
+++ Utils.swift
@@ -0,0 +1,13 @@
+//
+//  Utils.swift
+//  LearningApp
+//
+//  Created by Abdullah Kardaş on 18.06.2024.
+//
+
+import Foundation
+import UIKit
+
+let versionNumber = (Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString")  ?? "1.0" ) as! String
+let buildNumber = (Bundle.main.object(forInfoDictionaryKey: "CFBundleVersion")  ?? "1" ) as! String
+let deviceIdentifier = UIDevice.current.identifierForVendor!.uuidString
